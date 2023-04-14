450a38aaec1263558918bcf51e62d3ab5b59b6f7
Ashok Menon <ashok@mystenlabs.com>
2023-04-14 15:59:23 +0100

    Setting up dependencies between crates

--- crates/layer-0/src/lib.rs
+++ crates/layer-0/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[derive(Debug)]
+pub struct Layer0(pub u64);
+pub fn layer_0() -> Layer0 {
+    Layer0(42)
 }
--- crates/layer-2/src/main.rs
+++ crates/layer-2/src/main.rs
@@ -1,3 +1,9 @@
+macro_rules! test {
+    ($fn:ident) => { println!("{} = {:?}", stringify!($fn), $fn::$fn()) }
+}
+
 fn main() {
-    println!("Hello, world!");
+    test!(layer_0);
+    test!(layer_1_0);
+    test!(layer_1_1);
 }
