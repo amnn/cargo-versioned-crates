3e1ba82980ca354adad1f62089f7c2b5215e594f
Ashok Menon <ashok@mystenlabs.com>
2023-04-17 14:12:58 +0100

    Renaming: `layer-1` -> `layer-1-latest` and `layer-1-mux` -> `layer-1`
    Makes it easier for consumers of `layer-1` to refer to it (and not
    make the mistake of just referring to the latest version).

--- crates/layer-1/src/lib.rs
+++ crates/layer-1/src/lib.rs
@@ -1,4 +1,27 @@
-pub use ext_0::*;
-pub use ext_1::*;
-pub use layer_1_0::*;
-pub use layer_1_1::*;
+use ext_0::Ext0;
+use layer_0::Layer0;
+use layer_1_v1 as v1;
+
+pub fn ext_1(v: u8) -> Ext0 {
+    match v {
+        1 => v1::ext_1(),
+        // 2 => v2::ext_1(),
+        _ => unreachable!("ext_1: Unrecognized version, v{v}"),
+    }
+}
+
+pub fn layer_1_0(v: u8) -> Layer0 {
+    match v {
+        1 => v1::layer_1_0(),
+        // 2 => v2::layer_1_0(),
+        _ => unreachable!("layer_1: Unrecognized version, v{v}"),
+    }
+}
+
+pub fn layer_1_1(v: u8) -> Layer0 {
+    match v {
+        1 => v1::layer_1_1(),
+        // 2 => v2::layer_1_1(),
+        _ => unreachable!("layer_1: Unrecognized version, v{v}"),
+    }
+}
--- crates/layer-2/src/main.rs
+++ crates/layer-2/src/main.rs
@@ -1,6 +1,13 @@
 fn main() {
     println!("layer_0   = {:?}", layer_0::layer_0());
-    println!("ext_1     = {:?}", layer_1::ext_1());
-    println!("layer_1_0 = {:?}", layer_1::layer_1_0());
-    println!("layer_1_1 = {:?}", layer_1::layer_1_1());
+    println!("");
+    println!("Version 1:");
+    println!("ext_1     = {:?}", layer_1::ext_1(1));
+    println!("layer_1_0 = {:?}", layer_1::layer_1_0(1));
+    println!("layer_1_1 = {:?}", layer_1::layer_1_1(1));
+
+    // println!("Version 2:");
+    // println!("ext_1     = {:?}", layer_1::ext_1(2));
+    // println!("layer_1_0 = {:?}", layer_1::layer_1_0(2));
+    // println!("layer_1_1 = {:?}", layer_1::layer_1_1(2));
 }
